490c2657781b4db3131c824c3e3d2ce546b44dbd
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2021-04-19 17:23:28 +0800

    start on Lab2

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <cstdio>
+
 using namespace std;
 
 int main(void)
 {
+    int numLines;
     string s;
-    cout << "Input a string: ";
-    cin >> s;
-    cout << "You typed: " << s << endl;
+    getline(cin, s);
+    numLines = atoi(s.c_str());
+
+    for (int i = 0; i < numLines; i++)
+    {
+        // int pos1;
+        // int pos2;
+        // scanf("%d %d %s", &pos1, &pos2, s);
+        char s[80]; // limit of 80 characters
+        fgets(s, 80, stdin);
+        printf("You typed: %s", s);
+        // printf("Agent #%d is at (%d, %d)\n", i + 1, pos1, pos2);
+        // sprintf("Says: %d %d", s[0], s[2]);
+    }
+
     return 0;
 }
